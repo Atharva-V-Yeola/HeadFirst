b5caa3a97cb660c1da3db355f2a8438fe713d630
Atharva-V-Yeola <atharvavy24@gmail.com>
2024-12-28 13:54:30 +0530

    Create getters and setters.java

---
+++ getters and setters.java
@@ -0,0 +1,26 @@
+class EletroGuitar{
+	
+    String brand;
+    int numOfPickups;
+    boolean rockStarUsesIt;
+    
+    String getBrand(){
+    	return brand;
+    }
+    void setBrand(String aBrand){
+    	brand = aBrand;
+    }
+    int getNumOfPickups(){
+    	return numOfPickups;
+    }
+    void setNumOfPicks(int num){
+    	numOfPicks = num;
+    }
+    boolean getRockStarUsesIt(){
+    	return rockStarUsesIt;
+    }
+   	void setRockStarUsesIt(boolean yesOrNo){
+    	rockStarUsesIt = yesOrNo;
+    }
+    
+ }
